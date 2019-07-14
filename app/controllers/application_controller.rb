b64474b876231ff101bf12c44affcc525ab955e8
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-07-14 10:23:41 +0900

    feat: sorbetの型チェック用のコードを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 # typed: strong
 class ApplicationController < ActionController::Base
+  extend T::Sig
+
+  private
+  #
+  # sorbetテスト用のメソッド(Integer to String)
+  #
+  # @param [Integer] x
+  #
+  # @return [String]
+  #
+  sig {params(x: Integer).returns(String)}
+  def sorbet_test_to_s_app(x)
+    x.to_s
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,34 @@
 # typed: true
 class PagesController < ApplicationController
+  extend T::Sig
 
+  # typed: strong だとsignatureを定義しないとエラーとなる
   def about
+    # コメント化されている部分はSorbetによる型チェックエラーとなる
+    @value = ''
+    @value += sorbet_test_to_s CONST_INT
+    @value += sorbet_test_to_s CONST_FLOAT
+    # @value += sorbet_test_to_s CONST_STRING
+    @value += sorbet_test_to_s_app CONST_INT
+    # @value += sorbet_test_to_s_app CONST_FLOAT
+    # @value += sorbet_test_to_s_app CONST_STRING
   end
 
+  private
+
+  CONST_INT = 123
+  CONST_FLOAT = 45.6
+  CONST_STRING = '7-8-9'.freeze
+
+  #
+  # sorbetテスト用のメソッド(Integer to String)
+  #
+  # @param [Integer] x
+  #
+  # @return [String]
+  #
+  sig {params(x: T.any(Integer, Float)).returns(String)}
+  def sorbet_test_to_s(x)
+    x.to_s
+  end
 end
